e4b5419ad097dbc352220035e0452c9853296e52
Lizon <Paula.Lizon@lionbridge.com>
2020-11-03 11:38:31 +0100

    start

---
+++ index.js
@@ -0,0 +1,31 @@
+// const bubbleSort = (arr) => {
+
+//     let length = arr.length
+
+//     for (let i = (length - 1); i >= 0; i--) {
+//         for (let j = (length - 1); j > 0; j--) {
+//             if (arr[j] > arr[j + 1]) {
+//                 let a = arr[j]
+//                 let b = arr[j + 1]
+//                 arr[j] = b
+//                 arr[j + 1] = a
+//             }
+//         }
+//     }
+//     return arr
+// }
+
+
+// console.log(bubbleSort([1, - 6, 3, 4, 7, 9, 8, 19, 34, -5, -6,]))
+
+
+// const operation = (arr) => {
+// let zoo = ["bird", ...arr]
+// zoo.splice(2, 0, "snake")
+// return zoo
+// }
+
+
+// console.log(operation(["dog", "monkey", "cat"]))
+
+
