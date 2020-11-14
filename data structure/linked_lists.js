1b7ac528f2b597089ba85105d3f8d1a505a25afb
soygitana <lizon.paula@gmail.com>
2020-11-14 08:56:31 +0100

    solved

---
+++ data structure/linked_lists.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    const node = new Node(data, this.head);
+    this.head = node;
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+
+  getFirst() {
+    return this.head.data;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+}
+
+const list = new LinkedList();
+list.insertFirst("a");
+console.log(list);
+list.removeFirst();
+console.log(list.size()); // returns 0 after removing first element
