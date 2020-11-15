f58f5949d6fca9058879825d934799643673c0ae
soygitana <lizon.paula@gmail.com>
2020-11-15 19:57:56 +0100

    solved

--- algorithms/LINKED LISTS/3. from_tail.js
+++ algorithms/LINKED LISTS/3. from_tail.js
@@ -2,48 +2,52 @@
 // Without keeping a counter, return the value in a linked list that is at a given step away from the end.
 
 class LinkedListNode {
-  constructor(value, next) {
-    this.value = value;
-    this.next = next || null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  prepend(value) {
-    const newNode = new LinkedListNode(value, this.head);
-
-    this.head = newNode;
-    if (!this.tail) this.tail = newNode;
-    return this;
-  }
-
-  remove(key) {
-    let moveByOne = this.head;
-    let moveByKey = this.head;
-
-    while (key !== 0) {
-      key--;
-      moveByKey = moveByKey.next;
+    constructor(value, next) {
+      this.value = value;
+      this.next = next || null;
     }
-    while (moveByOne.next && moveByKey.next) {
-      moveByOne = moveByOne.next;
-      moveByKey = moveByKey.next;
-
-      if (moveByKey === this.tail) {
-        moveByOne.next = moveByKey;
+  }
+  
+  class LinkedList {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+  
+    prepend(value) {
+      const newNode = new LinkedListNode(value, this.head);
+  
+      this.head = newNode;
+      if (!this.tail) this.tail = newNode;
+      return this;
+    }
+  
+    remove(key) {
+      let moveByOne = this.head;
+      let moveByKey = this.head;
+  
+      while (key !== 0) {
+        key--;
+        moveByKey = moveByKey.next;
       }
+      while (moveByOne.next && moveByKey.next) {
+        moveByOne = moveByOne.next;
+        moveByKey = moveByKey.next;
+  
+        if (moveByKey === this.tail) {
+          moveByOne.next = moveByKey;
+        }
+       
+      }
+       return moveByOne
     }
   }
-}
-
-let list = new LinkedList();
-list.prepend(9);
-list.prepend(7);
-list.prepend(5);
-
-console.log(list.remove(3));
+  
+  let list = new LinkedList();
+  list.prepend(1);
+  list.prepend(2);
+  list.prepend(3);
+  list.prepend(4);
+  list.prepend(5);
+  
+  console.log(list.remove(2));
